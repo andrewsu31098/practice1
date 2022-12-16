4cba8a1c08f6a0d82177de3fd827f33442dbbcf5
Andrew Su <chronicles31098@gmail.com>
2022-12-15 16:52:18 -0800

    Small CSS Fixes. Carslist button and Landing object-position;

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+};
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/contact": { page: "/contact" },
+    };
+  },
+};
